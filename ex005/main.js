ca1ec339171eae0613809f047316c102fea83c97
Pauloprogramador2 <pauloricardoalmeidagomes@gmail.com>
2024-10-15 09:26:54 -0300

    teste Java

--- ex005/main.js
+++ ex005/main.js
@@ -1,4 +1,7 @@
 const form = document.getElementById('form-deposito')
+const nomeBeneficiario = document.getElementById('nome-beneficiario');
+let formEvalido = false;
+
 
 function validaNome(nomeCompleto){
     const nomeComoArray = nomeCompleto.split(' ');
@@ -6,14 +9,12 @@ function validaNome(nomeCompleto){
 }
 
 form.addEventListener('submit' , function(e){
-    let formEvalido = false;
  e.preventDefault();
 
-const nomeBeneficiario = document.getElementById('nome-beneficiario');
 const numeroContaBeneficiario = document.getElementById('numero-conta');
 const valorDeposito = document.getElementById('valor-deposito');
 const descricao = document.getElementById('descricao');
-const mensagemSucesso = `Montante de: <b>${valorDeposito.value}</b> depositado para o cliente: <b>${nomeBeneficiario.value}</b> - conta <b>${numeroContaBeneficiario.value}</b>`;
+const mensagemSucesso = `Montante de: <b>R$${valorDeposito.value}</b> depositado para o cliente: <b>${nomeBeneficiario.value}</b> - conta <b>${numeroContaBeneficiario.value}</b>`;
 
 formEvalido = validaNome(nomeBeneficiario.value)
 
@@ -21,16 +22,37 @@ formEvalido = validaNome(nomeBeneficiario.value)
 
 if (formEvalido){
 
+const containerMensagemSucesso = document.querySelector('.sucess-message')
+containerMensagemSucesso.innerHTML = mensagemSucesso;
+containerMensagemSucesso.style.display = 'block';
 
-document.querySelector('.sucess-message').innerHTML = mensagemSucesso;
+
 nomeBeneficiario.value = '';
 numeroContaBeneficiario.value = '';
 valorDeposito.value = '';
 descricao.value = '';
 }
  else {
- alert("O nome não está completo");
- }
+    nomeBeneficiario.style.border = '1px solid red';
+    document.querySelector('.error-message').style.display = 'block';
+}
 })
 
-console.log(form);
+nomeBeneficiario.addEventListener('keyup' , function(e){
+console.log(e.target.value);
+formEvalido = validaNome(e.target.value);
+if (!formEvalido){
+    nomeBeneficiario.classList.add('error');
+    //nomeBeneficiario.style.border = '1px solid red';
+    document.querySelector('.error-message').style.display = 'block';
+}
+else{
+    nomeBeneficiario.classList.remove('error');
+        
+        document.querySelector('.error-message').style.display = 'none';
+
+
+    }
+}
+);
+
